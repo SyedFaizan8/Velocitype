00d538f9e3730731593ecae2f1cbd34abda2c835
Faizan <syedfaizan.edu@gmail.com>
2025-01-20 21:46:31 +0530

    first commit

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, world!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ apps/frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-slate-800 grid place-items-center font-mono tracking-wider">
+      Hello world
+    </div>
+  )
+}
+
+export default Home
