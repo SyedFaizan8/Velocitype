ba5798f037d137b57adb521b33cde9ff7055df2b
Faizan <syedfaizan.edu@gmail.com>
2025-02-18 04:49:11 +0530

    changed data

--- apps/velocity/src/lib/prisma.ts
+++ apps/velocity/src/lib/prisma.ts
@@ -1,26 +1,9 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-// lib/prismaDynamic.ts
-
-
-import { PrismaClient } from '@prisma/client';
-
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
-} else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
-}
-
-export { prisma };
 
--- apps/velocity/src/types/global.d.ts
+++
@@ -1,6 +0,0 @@
-// global.d.ts or types/global.d.ts
-declare global {
-    var prisma: PrismaClient | undefined;
-}
-
-export { };
