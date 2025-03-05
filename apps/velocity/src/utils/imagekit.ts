06ec55ceeb94e422c5752d2bb0650e87955e3ed1
Faizan <syedfaizan.edu@gmail.com>
2025-03-05 22:32:42 +0530

    refactor imagekit module to export a factory function for instance creation

--- apps/velocity/src/utils/imagekit.ts
+++ apps/velocity/src/utils/imagekit.ts
@@ -3,10 +3,12 @@
 import ImageKit from "imagekit";
 import { NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY, NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT, IMAGEKIT_PRIVATE_KEY } from "@/utils/constants"
 
-const imagekit = new ImageKit({
-  publicKey: NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
-  privateKey: IMAGEKIT_PRIVATE_KEY,
-  urlEndpoint: NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
-});
+const createImageKitInstance = async () => {
+  return new ImageKit({
+    publicKey: NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
+    privateKey: IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint: NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
+  });
+};
 
-export default imagekit;
+export default createImageKitInstance;
